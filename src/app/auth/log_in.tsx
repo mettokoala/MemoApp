6e2e83625bd08ae4066f6fbc99ae919ee5b60b9f
yamakoala <52974545+mettokoala@users.noreply.github.com>
2024-03-17 17:53:45 +0900

    値を保持

--- src/app/auth/log_in.tsx
+++ src/app/auth/log_in.tsx
@@ -1,7 +1,7 @@
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native'
 
 import { Link, router } from 'expo-router'
-
+import { useState } from 'react'
 import Button from '../../components/Button'
 
 const handlePress = (): void => {
@@ -10,12 +10,30 @@ const handlePress = (): void => {
 }
 
 const LogIn = (): JSX.Element => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   return (
     <View style={styles.container}>
       <View style={styles.inner}>
         <Text>Log in</Text>
-        <TextInput style={styles.input} value='Email address' />
-        <TextInput style={styles.input} value='Password' />
+        <TextInput
+          style={styles.input}
+          value={email}
+          onChangeText={(text) => { setEmail(text) }}
+          autoCapitalize='none'
+          keyboardType='email-address'
+          placeholder='Email Address'
+          textContentType='emailAddress'
+        />
+        <TextInput
+          style={styles.input}
+          value={password}
+          onChangeText={(text) => { setPassword(text) }}
+          autoCapitalize='none'
+          secureTextEntry
+          placeholder='Password'
+          textContentType='password'
+        />
         <Button label='Submit' onPress={handlePress} />
         <View style={styles.footer}>
           <Text style={styles.footerText}>Not registered?</Text>
